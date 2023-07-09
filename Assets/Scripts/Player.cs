e5931bc74c8ca5335961f3289a9d44112c323546
Merc1073 <sergeikoblov2000@gmail.com>
2023-07-09 10:42:08 +0100

    Player variables

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    float maxHealth;
+
+    [SerializeField]
+    float currentHealth;
+
+
+    void Update()
+    {
+        
+    }
+}
